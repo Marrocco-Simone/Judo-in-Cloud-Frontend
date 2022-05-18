823221abc57d9497f6112d0c244432a7a21889e6
Ale Rayz <rayz1065@gmail.com>
2022-05-18 22:10:15 +0200

    Updated login page with call to backend, updated styles

--- src/Components/Layout/CenteredBox.tsx
+++ src/Components/Layout/CenteredBox.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 const CenteredBox: FC<any> = ({ children }) => {
   return (
-    <div className='w-full h-screen flex items-center justify-center'>
+    <div className='w-full min-h-screen flex items-center justify-center'>
       <div className='border dark:border-neutral-400 p-10 rounded-2xl dark:bg-neutral-900 bg-neutral-100'>
         {children}
       </div>
--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import OutlinedButton from '../../Components/Buttons/OutlinedButton';
 import Input from '../../Components/Inputs/Input';
 import CenteredBox from '../../Components/Layout/CenteredBox';
+import { apiPost } from '../../Services/Api/api';
+import { setToken } from '../../Services/Auth/token-service';
 
 function LoginPage() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  function login(event: React.MouseEvent) {
+  async function login(event: React.MouseEvent) {
     event.preventDefault();
-    navigate('/manage');
+    setLoading(true);
+
+    try {
+      const data = ({
+        username,
+        password
+      });
+      const res = await apiPost<{ access_token: string}>('v1/auth/login', data);
+      setToken(res.access_token);
+      navigate('/manage');
+    } catch (err) {
+      console.error({ err });
+    }
+    setLoading(false);
   }
 
   return (
@@ -20,15 +38,17 @@ function LoginPage() {
         </h2>
         <div className='mb-2'>
           <label>Nome utente</label>
-          <Input placeholder='Nome utente'></Input>
+          <Input placeholder='Nome utente' value={username}
+            onChange={event => setUsername(event.target.value)}></Input>
         </div>
         <div className='mb-2'>
           <label>Password</label>
-          <Input type='password' placeholder='*********'></Input>
+          <Input type='password' placeholder='*********' value={password}
+            onChange={event => setPassword(event.target.value)}></Input>
         </div>
 
         <div className='flex justify-end mt-5'>
-          <OutlinedButton type='submit' onClick={login}>
+          <OutlinedButton type='submit' onClick={login} disabled={loading}>
             Login
           </OutlinedButton>
         </div>
